83d4fc95eb3472bfa3c2b541c8ae84403704705a
Hetlr88 <mrhetlr0@gmail.com>
2024-08-24 19:09:30 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,13 +16,13 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1287276743
-    OWNER = config("OWNER")
+    APP_ID = config("APP_ID",29452145, cast=int)
+    API_HASH = config("API_HASH","5a2784e571fe5043852d32396a34a13b")
+    BOT_TOKEN = config("BOT_TOKEN","7445547849:AAHfX4_8TQ57cg71Q_l9ABoa6RfgfPWHgsE")
+    DEV = 6169288210
+    OWNER = config("OWNER","6169288210")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMB = config("THUMBNAIL")
+    THUMB = config("THUMBNAIL","https://b.top4top.io/p_315802pis0.jpg")
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
